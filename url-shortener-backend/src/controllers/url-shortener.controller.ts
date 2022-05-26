bf832553dfdac812ba2f28a19895830bb4d6f988
Paul Olabisi <pauloolabisi@gmail.com>
2022-05-26 15:07:40 +0100

    finished up backend

--- url-shortener-backend/src/controllers/url-shortener.controller.ts
+++ url-shortener-backend/src/controllers/url-shortener.controller.ts
@@ -17,11 +17,14 @@ import {
   requestBody,
   response,
   HttpErrors,
+  Response,
+  RestBindings,
 } from '@loopback/rest';
 import { Url } from '../models';
 import { UrlRepository } from '../repositories';
 import { nanoid } from 'nanoid'
 import * as yup from 'yup';
+import { inject } from '@loopback/core';
 
 let schema = yup.object().shape({
   longUrl: yup.string().trim().url().required(),
@@ -71,17 +74,6 @@ export class UrlShortenerController {
     return this.urlRepository.create(url);
   }
 
-  @get('/urls/count')
-  @response(200, {
-    description: 'Url model count',
-    content: { 'application/json': { schema: CountSchema } },
-  })
-  async count(
-    @param.where(Url) where?: Where<Url>,
-  ): Promise<Count> {
-    return this.urlRepository.count(where);
-  }
-
   @get('/urls')
   @response(200, {
     description: 'Array of Url model instances',
@@ -100,26 +92,7 @@ export class UrlShortenerController {
     return this.urlRepository.find(filter);
   }
 
-  @patch('/urls')
-  @response(200, {
-    description: 'Url PATCH success count',
-    content: { 'application/json': { schema: CountSchema } },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Url, { partial: true }),
-        },
-      },
-    })
-    url: Url,
-    @param.where(Url) where?: Where<Url>,
-  ): Promise<Count> {
-    return this.urlRepository.updateAll(url, where);
-  }
-
-  @get('/urls/{id}')
+  @get('/urls/{shortUrl}')
   @response(200, {
     description: 'Url model instance',
     content: {
@@ -129,46 +102,19 @@ export class UrlShortenerController {
     },
   })
   async findById(
-    @param.path.number('id') id: number,
-    @param.filter(Url, { exclude: 'where' }) filter?: FilterExcludingWhere<Url>
-  ): Promise<Url> {
-    return this.urlRepository.findById(id, filter);
+    @param.path.string('shortUrl') shortUrl: string,
+    @inject(RestBindings.Http.RESPONSE) response: Response
+  ) {
+
+    let result = await this.urlRepository.findOne({ where: { shortUrl: shortUrl }, limit: 1 });
+
+    if (result != null) {
+      return response.redirect(result.longUrl);
+    }
+    else {
+      throw new HttpErrors.NotFound('Short URL not found');
+    }
+
   }
 
-  @patch('/urls/{id}')
-  @response(204, {
-    description: 'Url PATCH success',
-  })
-  async updateById(
-    @param.path.number('id') id: number,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Url, { partial: true }),
-        },
-      },
-    })
-    url: Url,
-  ): Promise<void> {
-    await this.urlRepository.updateById(id, url);
-  }
-
-  @put('/urls/{id}')
-  @response(204, {
-    description: 'Url PUT success',
-  })
-  async replaceById(
-    @param.path.number('id') id: number,
-    @requestBody() url: Url,
-  ): Promise<void> {
-    await this.urlRepository.replaceById(id, url);
-  }
-
-  @del('/urls/{id}')
-  @response(204, {
-    description: 'Url DELETE success',
-  })
-  async deleteById(@param.path.number('id') id: number): Promise<void> {
-    await this.urlRepository.deleteById(id);
-  }
 }
